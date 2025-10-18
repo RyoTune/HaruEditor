fb51e57b15866027f1bf14face0906c896792eed
RyoTune <zutomayofan50@gmail.com>
2025-10-18 18:11:44 -0500

    I hate StackPanels.

---
+++ HaruEditor.Desktop/Converters/HateStackPanelConverter.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+
+namespace HaruEditor.Desktop.Converters;
+
+public class HateStackPanelConverter : IValueConverter
+{
+    public static HateStackPanelConverter Instance = new();
+    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Rect bounds)
+        {
+            return bounds.Height - 128;
+        }
+        
+        return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
