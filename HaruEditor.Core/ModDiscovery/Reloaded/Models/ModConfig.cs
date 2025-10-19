657a23291b74f5d96f93d76fc98c6a9b389ab6b3
RyoTune <zutomayofan50@gmail.com>
2025-10-19 11:17:17 -0500

    Add Reloaded mod discoverer.

---
+++ HaruEditor.Core/ModDiscovery/Reloaded/Models/ModConfig.cs
@@ -0,0 +1,8 @@
+namespace HaruEditor.Core.ModDiscovery.Reloaded.Models;
+
+public class ModConfig
+{
+    public string ModId { get; set; } = string.Empty;
+    public string ModName { get; set; } = string.Empty;
+    public string ModAuthor { get; set; } = string.Empty;
+}
---
+++ HaruEditor.Core/ModDiscovery/Reloaded/ReloadedModDiscoverer.cs
@@ -0,0 +1,49 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using HaruEditor.Core.ModDiscovery.Reloaded.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
+
+namespace HaruEditor.Core.ModDiscovery.Reloaded;
+
+public class ReloadedModDiscoverer
+{
+    private readonly ILogger<ReloadedModDiscoverer>? _log;
+    private readonly List<ModConfig> _mods = new();
+
+    public ReloadedModDiscoverer(ILogger<ReloadedModDiscoverer>? log)
+    {
+        _log = log;
+        
+        DiscoverMods();
+    }
+
+    public IReadOnlyList<ModConfig> Mods => _mods;
+
+    private void DiscoverMods()
+    {
+        if (TryGetModsDir(out var modsDir))
+        {
+            if (!Directory.Exists(modsDir)) return;
+
+            foreach (var modDir in Directory.EnumerateDirectories(modsDir))
+            {
+                var modConfigFile = Path.Join(modDir, "ModConfig.json");
+                if (!File.Exists(modConfigFile)) continue;
+
+                try
+                {
+                    var modConfig = JsonSerializer.Deserialize<ModConfig>(File.ReadAllBytes(modConfigFile)) ?? throw new();
+                    _mods.Add(modConfig);
+                }
+                catch (Exception ex) { _log?.LogError(ex, "Failed to load mod config."); }
+            }
+        }
+    }
+
+    private static bool TryGetModsDir([NotNullWhen(true)]out string? modsDir)
+    {
+        modsDir = Environment.GetEnvironmentVariable("RELOADEDIIMODS", EnvironmentVariableTarget.User);
+        return modsDir != null;
+    }
+}
