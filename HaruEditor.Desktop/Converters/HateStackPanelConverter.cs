1745e47235aca6b4f62cf7d25dca51997bd6b44f
RyoTune <zutomayofan50@gmail.com>
2025-11-04 06:54:21 -0600

    Adjust items ListView height.

--- HaruEditor.Desktop/Converters/HateStackPanelConverter.cs
+++ HaruEditor.Desktop/Converters/HateStackPanelConverter.cs
@@ -8,13 +8,13 @@ namespace HaruEditor.Desktop.Converters;
 
 public class HateStackPanelConverter : IValueConverter
 {
-    public static HateStackPanelConverter Instance = new();
+    public static readonly HateStackPanelConverter Instance = new();
     
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is Rect bounds)
         {
-            return bounds.Height - 128;
+            return bounds.Height - 180;
         }
         
         return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
